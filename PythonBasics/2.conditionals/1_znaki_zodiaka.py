1457bc194c6e2633d4c4129f7160661221a86fbd
Maxim <pmaxim@qeepnet.ru>
2020-07-30 20:44:41 +0300

    added input errors handling in 1 and 2 condit task

--- PythonBasics/2.conditionals/1_znaki_zodiaka.py
+++ PythonBasics/2.conditionals/1_znaki_zodiaka.py
@@ -1,64 +1,69 @@
 print('Знаки зодиака')
-month = input('Введите месяц')
-day = int(input('Введите число'))
-if day <= 31:
-    if month.lower() == 'январь':
-        if day <= 20:
-            print('Козерог')
-        elif day >= 21:
-            print('Водолей')
-    elif month.lower() == 'февраль':
-        if day <= 18:
-            print('Водолей')
-        elif day >= 19:
-            print('Рыбы')
-    elif month.lower() == 'март':
-        if day <= 20:
-            print('Рыбы')
-        elif day >= 21:
-            print('Овен')
-    elif month.lower() == 'апрель':
-        if day <= 20:
-            print('Овен')
-        elif day >= 21:
-            print('Телец')
-    elif month.lower() == 'май':
-        if day <= 21:
-            print('Телец')
-        elif day >= 22:
-            print('Близнецы')
-    elif month.lower() == 'июнь':
-        if day <= 21:
-            print('Близнецы')
-        elif day >= 22:
-            print('Рак')
-    elif month.lower() == 'июль':
-        if day <= 22:
-            print('Рак')
-        elif day >= 23:
-            print('Лев')
-    elif month.lower() == 'август':
-        if day <= 23:
-            print('Лев')
-        elif day >= 24:
-            print('Дева')
-    elif month.lower() == 'сентябрь':
-        if day <= 23:
-            print('Дева')
-        elif day >= 24:
-            print('Весы')
-    elif month.lower() == 'октябрь':
-        if day <= 23:
-            print('Весы')
-        elif day >= 24:
-            print('Скорпион')
-    elif month.lower() == 'ноябрь':
-        if day <= 22:
-            print('Скорпион')
-        elif day >= 23:
-            print('Стрелец')
-    elif month.lower() == 'декабрь':
-        if day <= 21:
-            print('Стрелец')
-        elif day >= 22:
-            print('Козерог')
+
+try:
+    month = input('Введите месяц')
+    day = int(input('Введите число'))
+    if day <= 31:
+        if month.lower() == 'январь':
+            if day <= 20:
+                print('Козерог')
+            elif day >= 21:
+                print('Водолей')
+        elif month.lower() == 'февраль':
+            if day <= 18:
+                print('Водолей')
+            elif day >= 19:
+                print('Рыбы')
+        elif month.lower() == 'март':
+            if day <= 20:
+                print('Рыбы')
+            elif day >= 21:
+                print('Овен')
+        elif month.lower() == 'апрель':
+            if day <= 20:
+                print('Овен')
+            elif day >= 21:
+                print('Телец')
+        elif month.lower() == 'май':
+            if day <= 21:
+                print('Телец')
+            elif day >= 22:
+                print('Близнецы')
+        elif month.lower() == 'июнь':
+            if day <= 21:
+                print('Близнецы')
+            elif day >= 22:
+                print('Рак')
+        elif month.lower() == 'июль':
+            if day <= 22:
+                print('Рак')
+            elif day >= 23:
+                print('Лев')
+        elif month.lower() == 'август':
+            if day <= 23:
+                print('Лев')
+            elif day >= 24:
+                print('Дева')
+        elif month.lower() == 'сентябрь':
+            if day <= 23:
+                print('Дева')
+            elif day >= 24:
+                print('Весы')
+        elif month.lower() == 'октябрь':
+            if day <= 23:
+                print('Весы')
+            elif day >= 24:
+                print('Скорпион')
+        elif month.lower() == 'ноябрь':
+            if day <= 22:
+                print('Скорпион')
+            elif day >= 23:
+                print('Стрелец')
+        elif month.lower() == 'декабрь':
+            if day <= 21:
+                print('Стрелец')
+            elif day >= 22:
+                print('Козерог')
+
+except Exception:
+    print('Month must be text and day must be an integer')
--- PythonBasics/2.conditionals/3_chocolate.py
+++ PythonBasics/2.conditionals/3_chocolate.py
@@ -1,9 +1,11 @@
+try:
+    n = int(input('Enter n: '))
+    m = int(input('Enter m: '))
+    k = int(input('Enter k: '))
 
-n = int(input('Enter n: '))
-m = int(input('Enter m: '))
-k = int(input('Enter k: '))
-
-if k < n * m and (k % n == 0 or k % m == 0):
-    print('True')
-else:
-    print('False')
+    if k < n * m and (k % n == 0 or k % m == 0):
+        print('True')
+    else:
+        print('False')
+except Exception:
+    print('Invalid input')
