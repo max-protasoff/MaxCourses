528ba82c480b140cf1c430f5b001653566ed8286
Maxim <pmaxim@qeepnet.ru>
2020-09-03 12:50:20 +0300

    decorators_hw_fix

--- AdvancedPythonAndDB/3.Decorators/decorator.py
+++ AdvancedPythonAndDB/3.Decorators/decorator.py
@@ -3,7 +3,7 @@ import datetime
 def logger(func):
     def wrapper(args_for_func):
         with open('log.txt', 'a', encoding='utf-8') as file:
-            file.write('Function called: ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
+            file.write(f'Function {func} called: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}. Args: {args_for_func}\n')
         func(args_for_func)
     return wrapper
 
